707c4853125540cde6db4ac55de84623db0e8ea0
Koroleva Alexandra <ale_alexandra@mail.ru>
2015-10-06 11:57:54 +0300

    kek

--- 6 nedelya/A.py
+++ 6 nedelya/A.py
@@ -1,1 +1,10 @@
-__author__ = 'student'
+N=int(input())
+k=1
+A=list(map(int, input().split()))
+for i in range (N):
+    a=A[i]
+    for j in range (k,N):
+        b=A[j]
+        if a==b:
+            print(a)
+    k+=1
